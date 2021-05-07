453d05f4d7b6c222a160848ff78ad1c6e4960c02
David Vasconcelos <dsv10@hotmail.com>
2021-05-07 20:05:25 -0300

    - modulo 6;

--- 4_5_ChildCancellation/src/main/kotlin/ChildCoroutines.kt
+++ 4_5_ChildCancellation/src/main/kotlin/ChildCoroutines.kt
@@ -3,27 +3,11 @@ import kotlinx.coroutines.experimental.*
 // 1. show that parent waits for children automatically
 // first run without a coroutineContext
 // then run with a coroutineContext
-fun main(args: Array<String>) = runBlocking {
-    var inner: Job? = null
-    val outer = launch {
-        launch {
-            repeat(2000) {
-                print(".")
-                delay(1)
-            }
-        }
-    }
-
-    outer.join()
-    println("Finished")
-}
-
- //1.a then run with a coroutineContext
 //fun main(args: Array<String>) = runBlocking {
-//    var inner: Job? = null
+//    //var inner: Job? = null
 //    val outer = launch {
-//        launch(coroutineContext) {
-//            repeat(2000) {
+//        launch {
+//            repeat(1000) {
 //                print(".")
 //                delay(1)
 //            }
@@ -31,5 +15,23 @@ fun main(args: Array<String>) = runBlocking {
 //    }
 //
 //    outer.join()
+//    println()
 //    println("Finished")
 //}
+
+//1.a then run with a coroutineContext
+fun main(args: Array<String>) = runBlocking {
+    //var inner: Job? = null
+    val outer = launch {
+        launch(coroutineContext) {
+            repeat(1000) {
+                print(".")
+                delay(1)
+            }
+        }
+    }
+
+    outer.join()
+    println()
+    println("Finished")
+}
